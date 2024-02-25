f3ef3bca221619a666176169087a09d6931da1af
DominiqueMarcil <madisonmarcil001@gmail.com>
2024-02-25 16:21:20 -0700

    Usernames and Passwords Specification

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+// const users = [
+//     { username: "Armaan", password: "Allidina" },
+//     { username: "Hans", password: "Bachman" },
+//     { username: "Christine", password: "Clark" },
+//     { username: "Tanis", password: "Crosschild" },
+//     { username: "Mick", password: "Jacobs" },
+//     { username: "Maxene", password: "Jubane" },
+//     { username: "Cyrus", password: "Lomibao" },
+//     { username: "Dominique", password: "Marcil" },
+//     { username: "Aliasgar", password: "Rajgara" },
+//     { username: "Arya", password: "Rod" },
+//     { username: "Tristan", password: "Scott" },
+//     { username: "Madisson", password: "Whelpley" },
+// ];
+
+// const loginForm = document.getElementById("loginForm");
+// const loginButton = document.getElementById("loginFormSubmit");
+// const loginErrorMsg = document.getElementById("loginErrorMsgPrompt");
+// const passwordField = document.getElementById("passwordField");
+// const showPasswordCheckbox = document.getElementById("showPassword");
+
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
+
+//     const users = users.find(user => user.username === username && user.password === password);
+
+
+//     if (users) {
+//         console.log("You have successfully logged in.");
+//         window.location.href = "home.html"; // Redirecting to other page.
+//     } else {
+//         loginErrorMsgPrompt.style.opacity = 1;
+//     }
+// })
+
+// showPasswordCheckbox.addEventListener("change", () => {
+//     const passwordFieldType = showPasswordCheckbox.checked ? "text" : "password";
+//     passwordField.type = passwordFieldType;
+// });
 const users = [
     { username: "Armaan", password: "Allidina" },
     { username: "Hans", password: "Bachman" },
@@ -24,15 +65,15 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    const user = users.find(user => user.username === username && user.password === password);
+    const foundUser = users.find(user => user.username === username && user.password === password);
 
-    if (users) {
+    if (foundUser) {
         console.log("You have successfully logged in.");
         window.location.href = "home.html"; // Redirecting to other page.
     } else {
-        loginErrorMsg.style.opacity = 1;
+        loginErrorMsg.style.opacity = 1; // Corrected the ID
     }
-})
+});
 
 showPasswordCheckbox.addEventListener("change", () => {
     const passwordFieldType = showPasswordCheckbox.checked ? "text" : "password";
