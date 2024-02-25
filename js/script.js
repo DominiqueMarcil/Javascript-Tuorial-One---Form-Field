eeb7aaf92e44d4490b1a699d63cd10a193e774e3
DominiqueMarcil <madisonmarcil001@gmail.com>
2024-02-25 15:14:25 -0700

    Password View

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
+const users = [
+    { username: "Armaan", password: "Allidina" },
+    { username: "Hans", password: "Bachman" },
+    { username: "Christine", password: "Clark" },
+    { username: "Tanis", password: "Crosschild" },
+    { username: "Mick", password: "Jacobs" },
+    { username: "Maxene", password: "Jubane" },
+    { username: "Cyrus", password: "Lomibao" },
+    { username: "Dominique", password: "Marcil" },
+    { username: "Aliasgar", password: "Rajgara" },
+    { username: "Arya", password: "Rod" },
+    { username: "Tristan", password: "Scott" },
+    { username: "Madisson", password: "Whelpley" },
+];
+
 const loginForm = document.getElementById("loginForm");
 const loginButton = document.getElementById("loginFormSubmit");
 const loginErrorMsg = document.getElementById("loginErrorMsgPrompt");
+const passwordField = document.getElementById("passwordField");
+const showPasswordCheckbox = document.getElementById("showPassword");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "m" && password === "mm") {
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (users) {
         console.log("You have successfully logged in.");
         window.location.href = "home.html"; // Redirecting to other page.
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+showPasswordCheckbox.addEventListener("change", () => {
+    const passwordFieldType = showPasswordCheckbox.checked ? "text" : "password";
+    passwordField.type = passwordFieldType;
+});
